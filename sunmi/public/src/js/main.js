1adbd2effdee808d626ed67cf0c6d26df750e48d
jumikim <rlawnal5984@gmail.com>
2019-08-30 00:08:40 +0900

    no message

--- sunmi/public/src/js/main.js
+++ sunmi/public/src/js/main.js
@@ -2,7 +2,13 @@ $(function() {
 
 	// select-box
 	$('.select-box button').on('click', function (){
-		$(this).parent().next().slideDown();
+		if(!$(this).hasClass('open')){
+			$(this).addClass('open');
+			$(this).parent().next().slideDown();
+		} else {
+			$(this).removeClass('open');
+			$(this).parent().next().slideUp();
+		}
 	});
 	$('.select-box li a').on('click', function (){
 		$(this).parent().parent().slideUp();
@@ -18,6 +24,18 @@ $(function() {
 		$('.mobile-nav').animate({'top': '-100%'}, 500);
 		$('html').removeClass('nav-open');
 	});
+
+	$(window).on('scroll', function(){
+		// console.log('z')
+		var scroll = $(window).scrollTop();
+		if (scroll >= 30) {
+			$('#p-wrapper').addClass('hidden');
+		} else if(scroll <= 30) {
+			$('#p-wrapper').removeClass('hidden');
+		}
+	});
+
+	
 
 });
 
