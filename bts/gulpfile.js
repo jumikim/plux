d8966962876713334c3aeacd3cd8716d72ee7f1f
jumikim <rlawnal5984@gmail.com>
2019-09-04 04:08:34 +0900

    no message

---
+++ bts/gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+// sass
+gulp.task('sass', async function() {
+    return gulp
+        .src('./public/src/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/src/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// html browser sync
+gulp.task('html', function() {
+    return gulp.src('./**/*.html')
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+gulp.task('browserSync', gulp.series('html', function(){
+    return browserSync.init({
+        port: 3333,
+        server: {
+            baseDir: './public/src/'
+        }
+    });
+}));
+
+// watch
+gulp.task('watch', async function() {
+    gulp.watch('./public/src/**/*.html', gulp.parallel('html'));
+    gulp.watch('./public/src/**/*.scss', gulp.parallel('sass'));
+});
+
+// all task
+gulp.task('start', gulp.parallel('browserSync', 'watch', 'sass'));
+
+
---
+++ bts/public/src/js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+
+	// select-box
+	$('.select-box button').on('click', function (){
+		if(!$(this).hasClass('open')){
+			$(this).addClass('open');
+			$(this).parent().next().slideDown();
+		} else {
+			$(this).removeClass('open');
+			$(this).parent().next().slideUp();
+		}
+	});
+	$('.select-box li a').on('click', function (){
+		$(this).parent().parent().slideUp();
+	});
+
+	$(window).on('scroll', function(){
+		var scroll = $(window).scrollTop();
+		var screenHalf = $(document).height() / 2;
+		if (scroll >= 30) {
+			$('#p-wrapper').addClass('hidden');
+		} else if(scroll <= 30) {
+			$('#p-wrapper').removeClass('hidden');
+		}
+
+		if(scroll >= screenHalf){
+			$('.join-box').addClass('floating');
+		} else if (scroll <= screenHalf){
+			$('.join-box').removeClass('floating');
+		}
+	});
+
+	// top button
+	$('.top').on('click', function(){
+		$('html, body').animate({scrollTop : 0}, 400);
+	});
+
+	
+
+});
+
+
+
